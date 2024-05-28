b22ded57869d4b545a9c0e1ae26e085e3c4a8b63
Shivam <singhkumarshivam15@gmail.com>
2024-05-28 13:37:32 +0530

    trying to deploy

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ console.log("hi",path.join(__dirname1,"../frontend/build"))
 if(process.env.NODE_ENV==='production'){
 app.use(express.static(path.join(__dirname1,"../frontend/build")))
 app.get('*',(req,res)=>{
-res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
+res.sendFile(path.resolve(__dirname1,"../frontend","build","index.html"))
 })
 }else{
   app.get("/",(req,res)=>{
