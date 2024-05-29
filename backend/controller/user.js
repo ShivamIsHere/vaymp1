c5df11342a0a69ddf751c2814625e4abdaa96890
Shivam <singhkumarshivam15@gmail.com>
2024-05-29 16:01:58 +0530

    trying to deploy

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -161,7 +161,7 @@ router.get(
       res.cookie("token", null, {
         expires: new Date(Date.now()),
         httpOnly: true,
-        sameSite: "none",
+        sameSite: "None",
         secure: true,
       });
       res.status(201).json({
