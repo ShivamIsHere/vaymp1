4a63bd1297ba2d0b23c7fcf93263d7c005259692
Shivam Kumar <singhkumarshivam15@gmail.com>
2024-06-10 13:31:11 +0530

    trying to deploy

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(cors(corsOptions));
 // Ensure preflight requests are handled
 app.options('*', cors(corsOptions));
 
-app.use(express.json());
+// app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
