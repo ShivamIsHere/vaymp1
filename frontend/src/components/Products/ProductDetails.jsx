52cffece644361e08abab106cdcd265760dba336
Shivam Kumar <singhkumarshivam15@gmail.com>
2024-07-26 03:17:27 +0530

    Update ProductDetails.jsx

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -455,12 +455,7 @@ const ProductDetails =  ({ data }) => {
                             title="Add to wishlist"
                           />
                         )}
-                        <RiShareForwardLine
-                    size={34}
-                    className="ml-2 text-blue-400"
-                    title="Share this product"
-                    onClick={copyToClipboard}
-                  />
+                        
                       </span>
                     </div>
                   </div>
